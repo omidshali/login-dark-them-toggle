1ca850102173cc9b4ffaf1ab7e62313c1376855e
omid <o.shali01@gmail.com>
2024-07-01 18:30:43 +0330

    created login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
+import React from "react";
 import "./index.css";
 import useLocalStorage from "use-local-storage";
 
 function App() {
+  const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "light");
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
   return (
-    <div className="App">
-      <h1>Login whith dark them</h1>
+    <div className="app" data-theme={theme}>
+      <div className="login">
+        <h1>Logon</h1>
+        <div className="container">
+          <div className="top">
+            <i class="fab fa-google"></i>
+            <i class="fab fa-facebook-square"></i>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-twitter-square"></i>
+            <i class="fab fa-apple"></i>
+          </div>
+          <p className="divider">
+            <span>Or</span>
+          </p>
+          <form>
+            <label>E-mail</label>
+            <input type="email" placeholder="Enter your email" />
+            <label>Password</label>
+            <input type="password" placeholder="Enter your passwprd" />
+            <div className="remember">
+              <input type="checkbox" checked="checked" />
+              <p>Remember Me</p>
+            </div>
+            <button>Log In</button>
+          </form>
+          <div className="bottom">
+            <p>Forget your password?</p>
+            <a href="/">Reset Password</a>
+          </div>
+          <p className="create">Create Account</p>
+        </div>
+        <div className="them-toggle">
+          <h2>Light Theme</h2>
+          <i onClick={switchTheme} class="fas fa-toggle-on"></i>
+        </div>
+      </div>
     </div>
   );
 }
